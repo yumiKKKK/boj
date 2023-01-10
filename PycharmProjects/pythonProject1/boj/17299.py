eb9e5b3205091b2ef5e4b46750f90d7b670dc278
yumiKKKK <121678395+yumiKKKK@users.noreply.github.com>
2023-01-10 09:11:02 +0900

    Update 17299.py

--- PycharmProjects/pythonProject1/boj/17299.py
+++ PycharmProjects/pythonProject1/boj/17299.py
@@ -1,21 +1,19 @@
-n=int(input())
-a=list(map(int, input().split()))
+from collections import Counter
+from sys import stdin
+n=int(stdin.readline())
+a=list(map(int, stdin.readline().split()))
 #등장횟수
-F=[]
-for i in a:
-    F.append(a.count(i))
-#오등큰수
-ngf=[]
-for i in range(n-1):
-    k=i
-    for j in range(i+1, n):
-        if F[j]>F[k]:
-            k=j
-            print(a[k], end=" ")
-            break
-    if k==i:
-        print(-1, end=" ")
+F=Counter(a)
 
+stack=[]
+ans=[-1]*n
+#오등큰수
+for i in range(n):
+    while(stack and F[a[stack[-1]]]<F[a[i]]):
+        ans[stack.pop()]=a[i]
+    stack.append(i)
+
+print(*ans)
 
 
 
